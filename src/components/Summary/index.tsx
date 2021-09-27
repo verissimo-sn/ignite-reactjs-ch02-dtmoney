f77c14281aa8800c457b9e00dacafe80e80fecb1
verissimo-sn <verissimo_sn@hotmail.com>
2021-09-27 09:29:27 -0300

    style: stylize summary card

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,9 +1,35 @@
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import totalImg from '../../assets/total.svg';
+
 import { Container } from './styles';
 
 export const Summary= () => {
   return (
     <Container>
-      <h1>Summary</h1>
+      <div className="">
+        <header>
+          <p>Entradas</p>
+          <img src={incomeImg} alt="Entradas" />
+        </header>
+        <strong>R$1000,00</strong>
+      </div>
+
+      <div className="">
+        <header>
+          <p>Saidas</p>
+          <img src={outcomeImg} alt="Saidas" />
+        </header>
+        <strong>- R$500,00</strong>
+      </div>
+
+      <div className="highlight-background">
+        <header>
+          <p>Total</p>
+          <img src={totalImg} alt="Total" />
+        </header>
+        <strong>R$500,00</strong>
+      </div>
     </Container>
   );
 }
