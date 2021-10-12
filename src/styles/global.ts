bf410088a729bd8638c756bfe005c8b71b79a342
verissimo-sn <verissimo_sn@hotmail.com>
2021-10-12 08:47:05 -0300

    chore: add miragejs

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -38,7 +38,7 @@ export const GlobalStyle = createGlobalStyle`
 
   body, input, textarea, button {
     font-family: 'Poppins', sans-serif;
-    font-weight: 400px;
+    font-weight: 400;
   }
 
   h1, h2, h3, h4, h5, h6, strong {
