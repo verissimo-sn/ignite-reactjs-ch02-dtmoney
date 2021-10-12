527b8bf75ddcc7fabac69365b311945dc213fa68
verissimo-sn <verissimo_sn@hotmail.com>
2021-10-12 09:21:14 -0300

    feat: create transaction modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,35 @@
+import { useState} from 'react';
+import Modal from 'react-modal';
+
 import logoImg from '../../assets/logo.svg';
 
 import { Container, Content } from './styles';
 
 export const Header = () => {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  const handleOpenNewTransactionModal = () => {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  const handleCloseNewTransactionModal = () => {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="dt money" />
-        <button type="button">
+        <button type="button" onClick={handleOpenNewTransactionModal}>
           Nova transação
         </button>
+
+        <Modal 
+          isOpen={isNewTransactionModalOpen} 
+          onRequestClose={handleCloseNewTransactionModal}
+        >
+          <h2>Cadastrar transação</h2>
+        </Modal>
       </Content>
     </Container>
   );
