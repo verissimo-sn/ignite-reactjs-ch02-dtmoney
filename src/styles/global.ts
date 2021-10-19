65d9e74a1f9084dbded419c4fc69634c1e7b4007
verissimo-sn <jose.neto@minu.co>
2021-10-19 09:27:11 -0300

    style: added modal close button styling

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -70,10 +70,24 @@ export const GlobalStyle = createGlobalStyle`
 
   .react-model-content {
     width: 100%;
-    max-width: 576%;
+    max-width: 576px;
     background: var(--background);
     padding: 3rem;
     position: relative;
     border-radius: 0.24rem;
+  }
+
+  .react-modal-close {
+    position: absolute;
+    right: 1.5rem;
+    top: 1.5rem;
+    border: 0;
+    background: transparent;
+
+    transition: filter 0.2s;
+
+    &:hover {
+      filter: brightness(0.8);
+    }
   }
 `;
