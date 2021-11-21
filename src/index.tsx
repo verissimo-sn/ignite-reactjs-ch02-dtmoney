460f0baeee0e8f0b75d88b3025174be179176c9e
verissimo-sn <verissimo_sn@hotmail.com>
2021-11-21 08:47:50 -0300

    feat: add transactions seed

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,29 @@ createServer({
     transaction: Model
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelancer de website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          created: new Date('2021-02-12 09:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'apto',
+          amount: 800,
+          created: new Date('2021-02-20 09:00:00'),
+        }
+      ],
+    });
+  },
+
   routes() {
     this.namespace = 'api';
 
