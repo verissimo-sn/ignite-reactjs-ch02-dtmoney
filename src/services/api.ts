6990e988ce6d40b5118b175090f00e63e7277e9c
verissimo-sn <verissimo_sn@hotmail.com>
2021-11-21 08:42:18 -0300

    feat: create transaction schema and create transaction route

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,20 @@
 import axios from 'axios';
 
+interface TransactionDataProps {
+  title: string;
+  value: number;
+  category: string;
+  type: string;
+}
+
 export const api = axios.create({
   baseURL: 'http://localhost:3000/api',
 });
+
+export const createTransaction = async (data: TransactionDataProps) => {
+  const result = await api.post('/transactions', {
+    ...data
+  });
+
+  return result.data;
+}
